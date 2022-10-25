a95dc2cf65ace6cf68c1cb0d5fd8c85f2ee08a99
Steve <stephenkago10@gmail.com>
2022-10-25 15:55:46 +0300

    Done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,7 +2,10 @@ require 'pry'
 
 class Triangle
   #write code here
+  
+  attr_accessor :side1, :side2, :side3
   @@sides = []
+
   def initialize(side1,side2,side3)
     @@sides = [side1,side2,side3]
     @@sides.sort!
